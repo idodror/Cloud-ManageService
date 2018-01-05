9d50bf018753b0bfb0408c912243c3614b647f73
morisamon <amonmoris@gmail.com>
2018-01-05 16:06:44 +0200

    added port definition

--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ namespace ManageService
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://localhost:5030")
                 .Build();
     }
 }
