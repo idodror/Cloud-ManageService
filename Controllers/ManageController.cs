3658bd9d2f3483be330c4a250f1f2271e962c93b
morisamon <amonmoris@gmail.com>
2018-01-06 18:45:38 +0200

    TokenValidation added

--- Controllers/ManageController.cs
+++ Controllers/ManageController.cs
@@ -20,24 +20,55 @@ namespace ManageService.Controllers
     [Route("api/[controller]")]
     public class ManageController : Controller {
         IBusClient client;
+        IDatabase cachingDB;
 
-        public ManageController (IBusClient _client) {
+        public ManageController (IBusClient _client, IRedisConnectionFactory caching) {
             client = _client;
+            cachingDB = caching.Connection().GetDatabase();
+        }
+
+                [HttpGet]
+        [Route("/ReadFromCache/{id}")]
+        public Data ReadFromCache(string id) {
+            Data d = Newtonsoft.Json.JsonConvert.DeserializeObject<Data>(cachingDB.StringGet(id.ToString()));
+            return d;
+        }
+
+                private bool VerifyTheToken(string id)
+        {
+            int index = id.LastIndexOf(':');
+            string newUserId = id.Substring(index + 1, id.Length - index - 1);
+            Data getData = ReadFromCache("id:" + newUserId);
+
+             if ((DateTime.Now - getData.create.AddSeconds(getData.ttl)) < TimeSpan.FromHours(1))
+             {
+                 return true;
+             }
+             else
+             {
+                 return false;
+             }
         }
 
         [HttpPost]
         [Route("/ShareFile")]
         public async Task<int> ShareFile([FromBody] ShareFile sf) {
-            ShareFileNoRev shareNoRev = new ShareFileNoRev(sf);
+            if(VerifyTheToken(sf.imgId)){
+                ShareFileNoRev shareNoRev = new ShareFileNoRev(sf);
 
-            // check if toUser exist
-            var response = await CouchDBConnect.PostToDB(shareNoRev, "shares");
-            
-            // create the same file with diffrenet id
-            await client.PublishAsync(shareNoRev);
+                // check if toUser exist
+                var response = await CouchDBConnect.PostToDB(shareNoRev, "shares");
+                
+                // create the same file with diffrenet id
+                await client.PublishAsync(shareNoRev);
 
-            Console.WriteLine(response);
-            return 1;
+                Console.WriteLine(response);
+                return 1;
+            }
+            else
+            {
+                return 1;
+            }
         }
     }
 }
--- Models/Data.cs
+++ Models/Data.cs
@@ -2,8 +2,10 @@ using System;
 
 namespace ManageService.Models {
     public class Data {
-        public int id { get; set; }
-        public string name { get; set; }
+        public string userID { get; set; }
+        public string tokenID { get; set; }
+        public int ttl { get; set; }
+        public DateTime create { get; set; }
 
     }
 }
